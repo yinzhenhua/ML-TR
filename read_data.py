fd8ef3bc7c27091b769eb066249fdab55d848635
Leo.yin <yinzhen_hua@163.com>
2019-05-22 17:31:36 +0800

    最终版本

--- read_data.py
+++ read_data.py
@@ -4,9 +4,12 @@
 """
 
 import pandas as pd
+import numpy as np
 
 def read_aqi():
     aqi_data = pd.read_csv("aqi2.csv")
+    cols = ['PM2.5', 'PM10', 'CO', 'No2', 'So2', 'O3']
     label = aqi_data["AQI"].values
-    x = aqi_data[['PM2.5', 'PM10', 'CO', 'No2', 'So2', 'O3']].values
+    x = aqi_data[cols]
+    x = (x-np.min(x))/(np.max(x)-np.min(x))
     return x.reshape(-1, 6), label.reshape(-1, 1)
--- show.py
+++ show.py
@@ -4,6 +4,8 @@
 """
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 def show_cost(costs):
     """
@@ -12,3 +14,19 @@ def show_cost(costs):
     sns.set_style("whitegrid")  
     plt.plot(costs)  
     plt.show()  
+
+
+def read_and_show_aqi():
+    aqi_data = pd.read_csv("aqi2.csv")
+    sns.set(style='whitegrid', context='notebook')
+    cols = ['PM2.5', 'PM10', 'CO', 'No2', 'So2', 'O3']
+    sns.pairplot(aqi_data[cols], size=2.5)
+    plt.show()
+
+    cm = np.corrcoef(aqi_data[cols].values.T)
+    sns.set(font_scale=1.5)
+    sns.heatmap(cm, cbar=True, annot=True, square=True,
+    fmt='.2f', annot_kws={'size':15}, yticklabels=cols, xticklabels=cols)
+    plt.show()
+
+#read_and_show_aqi()
